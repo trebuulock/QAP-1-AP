e0542b0df93efafd1212a6d476b3bfdd052fcd19
trebuulock <119910404+trebuulock@users.noreply.github.com>
2023-08-18 17:08:43 -0230

    Add files via upload

---
+++ src/TestDate.java
@@ -0,0 +1,9 @@
+public class TestDate {
+    public static void main(String[] args) {
+        // Create a Date object
+        Date date = new Date(18, 8, 2023);
+
+        // Print the date using toString() method
+        System.out.println("Date: " + date.toString());
+    }
+}
